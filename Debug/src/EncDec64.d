b7002897cc056ebb5625e253cac60a6a17c717a0
Christian Angelone <christiangelone@gmail.com>
2017-03-25 11:34:58 -0700

    Adds decode/encode implementations and std encoding&decoding

--- Debug/src/EncDec64.d
+++ Debug/src/EncDec64.d
@@ -1,5 +1,5 @@
 src/EncDec64.d: ../src/EncDec64.c ../src/actions.h ../src/about.h \
- ../src/options_list.h ../src/options.h
+ ../src/options_list.h ../src/options.h ../src/encdec.h
 
 ../src/actions.h:
 
@@ -8,3 +8,5 @@ src/EncDec64.d: ../src/EncDec64.c ../src/actions.h ../src/about.h \
 ../src/options_list.h:
 
 ../src/options.h:
+
+../src/encdec.h:
